2f74ea611baf96f95ee75a4c499c6367c23d886c
SergeyK <serega_konchakov@mail.ru>
2022-11-20 13:00:17 +0300

    Вывод третьей цифры заданного числа и сообщение об ошибке, если такой нет

---
+++ Вывод третьей цифры заданного числа/Program.cs
@@ -0,0 +1,15 @@
+﻿// Вывод второй цифры заданного числа или сообщение об ошибке
+void Zadacha13()
+{
+Console.WriteLine ("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number/100 != 0)
+{
+    number = number%100;
+    number = number%10;
+    Console.WriteLine(number);
+}
+else Console.WriteLine ("Третьей цифры нет");
+}
+
+Zadacha13();
