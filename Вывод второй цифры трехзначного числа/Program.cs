99812ae9ec45c088835ac8d2f95b19fb92d7aba1
SergeyK <serega_konchakov@mail.ru>
2022-11-20 12:58:16 +0300

    Вывод второй цифры трехзначного числа

---
+++ Вывод второй цифры трехзначного числа/Program.cs
@@ -0,0 +1,16 @@
+﻿// Вывод второй цифры трехзначного числа
+void Zadacha10()
+{
+Console.WriteLine ("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number/100 != 0)
+{
+    number = number%100;
+    number = number - number%10;
+    number = number/10;
+    Console.WriteLine(number);
+}
+else Console.WriteLine ("Число не является трехзначным, введите другое число");
+}
+
+Zadacha10();
