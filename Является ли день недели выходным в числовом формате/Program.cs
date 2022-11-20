e1de155eb202630fe7dfa0e12c08366bd3295da6
SergeyK <serega_konchakov@mail.ru>
2022-11-20 13:02:26 +0300

    Ввод дня недели в числовом формате и вывод сообщения, является ли он выходным днем

---
+++ Является ли день недели выходным в числовом формате/Program.cs
@@ -0,0 +1,9 @@
+﻿// Вводим цифру, обозначающую день недели, и проверяем, является ли этот день выходным
+Console.WriteLine ("Введите день недели цифрой:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0 && number < 8)
+{
+    if (number == 6 || number == 7) Console.WriteLine ("Да, это выходной!");
+    else Console.WriteLine ("Неа, не выходной!");
+}
+else Console.WriteLine ("Введите значение от 1 до 7");
